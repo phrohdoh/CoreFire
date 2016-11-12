c3bb2a43e69386dfe80801cea5e7d04e9d1e1cd2
Taryn Hill <Phrohdoh@gmail.com>
2016-11-12 08:20:27 -0600

    Initial commit

---
+++ Library.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Net.Http;
+
+namespace ClassLibrary
+{
+    public class Class1
+    {
+        public string MakeSyncHttpGet(string url)
+        {
+            using (var client = new HttpClient())
+                return client.GetStringAsync(url).Result;
+        }
+    }
+}
