d010d98f9a77ac4d8faf302f573b0fa81c8fac9b
Taryn Hill <Phrohdoh@gmail.com>
2016-11-12 12:16:16 -0600

    Add FireClientBuilder and use it in CoreFireConsoleApp

--- src/CoreFire/FireClient.cs
+++ src/CoreFire/FireClient.cs
@@ -3,19 +3,50 @@ using System.Collections.Generic;
 
 namespace CoreFire
 {
+    public class FireClientBuilder
+    {
+        string url;
+        string authToken;
+
+        // We do not want any public ctors.
+        FireClientBuilder() { }
+
+        public static FireClientBuilder Create() => new FireClientBuilder();
+
+        /// <param name="url">The firebase URL of your db.</param>
+        /// <c>FireClient.Create().WithUrl("http://your-db.firebaseio.com");</c>
+        public FireClientBuilder WithUrl(string url)
+        {
+            this.url = url;
+            return this;
+        }
+
+        /// <param name="url">The firebase URL of your db.</param>
+        /// <c>FireClient.Create().WithAuth("spqiQHnlwA6uS6Ur8H3ZrJinHbX951DzDySazIA");</c>
+        public FireClientBuilder WithAuth(string authToken)
+        {
+            this.authToken = authToken;
+            return this;
+        }
+
+        public FireClient Build()
+        {
+            return new FireClient
+            {
+                Url = url,
+                AuthToken = authToken,
+            };
+        }
+    }
+
     public class FireClient
     {
         public readonly Dictionary<string, string> RequestParams = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
 
         public string Url;
+        public string AuthToken;
 
-        /// <summary></summary>
-        /// <param name="url">The firebase URL of your db.</param>
-        /// <c>new FireClient("https://your-db.firebaseio.com/")</c>
-        public FireClient(string url)
-        {
-            Url = url;
-        }
+        internal FireClient() { }
 
         public bool IsValid()
         {
--- src/CoreFireConsoleApp/Program.cs
+++ src/CoreFireConsoleApp/Program.cs
@@ -5,7 +5,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        var client = new CoreFire.FireClient(null);
+        if (args.Length != 2)
+        {
+            Console.WriteLine("Please supply <url> <auth> in that order.");
+            Console.WriteLine("  CoreFireConsoleApp.exe https://your-db.firebaseio.com/ spqiQHnlwA6uS6Ur8H3ZrJinHbX951DzDySazIA");
+            Environment.Exit(1);
+        }
+
+        var url = args[0];
+        var auth = args[1];
+
+        var client = FireClientBuilder.Create()
+            .WithUrl(url)
+            .WithAuth(auth)
+            .Build();
+
         Console.WriteLine(client.IsValid());
     }
 }
